ea7843eaae6379fb3d3fbdbb7465670660b66ee3
kwansing14 <kwansing@hotmail.sg>
2020-07-03 13:16:51 +0800

    first commit for web-test

---
+++ app/javascript/react/app.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './app.css';
+
+function App() {
+  return (
+    <div className="App">
+        <h2>Hello React !</h2>
+    </div>
+  );
+}
+
+export default App;
